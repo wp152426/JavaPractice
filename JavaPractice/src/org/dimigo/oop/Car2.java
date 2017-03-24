4363ef89137a49a7c3d6063e9abbf07eb6c904ba
임형준 <@DESKTOP-FGQNPJR>
2017-03-24 11:46:41 +0900

    자바 실습과제 4. Car2

---
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ * Car
+ *
+ * 1. Outline :
+ * 2. Date : 2017. 3. 23.
+ * </pre>
+ *
+ * @author ¿”«¸¡ÿ
+ * @version : 1.0
+ */
+public class Car2 {
+	
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private long price;
+	/**
+	 * @return the company
+	 */
+	
+	public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, long newPrice){
+		company = newCompany;
+		model = newModel;
+		color = newColor;
+		maxSpeed = newMaxSpeed;
+		price = newPrice;
+	}
+	
+	
+	public String getCompany() {
+		return company;
+	}
+	/**
+	 * @param company the company to set
+	 */
+	public void setCompany(String newCompany) {
+		company = newCompany;
+	}
+	/**
+	 * @return the model
+	 */
+	public String getModel() {
+		return model;
+	}
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(String newModel) {
+		model = newModel;
+	}
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+	/**
+	 * @return the maxSpeed
+	 */
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	/**
+	 * @param maxSpeed the maxSpeed to set
+	 */
+	public void setMaxSpeed(int newMaxSpeed) {
+		maxSpeed = newMaxSpeed;
+	}
+	/**
+	 * @return the price
+	 */
+	public long getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(long newPrice) {
+		price = newPrice;
+	}
+	
+}
---
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ * CarTest
+ *
+ * 1. Outline :
+ * 2. Date : 2017. 3. 23.
+ * </pre>
+ *
+ * @author ¿”«¸¡ÿ
+ * @version : 1.0
+ */
+public class CarTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Car2 c1 = new Car2("Hyundai", "Genesis", "Black", 225, 50000000);
+		Car2 c2 = new Car2("Kia", "K7", "White", 246, 40000000);
+		Car2 c3 = new Car2("Samsung", "SM7", "Gray", 200, 38000000);
+		
+		System.out.println("<< Car List >>");
+		System.out.println("Company : " + c1.getCompany());
+		System.out.println("Model : " + c1.getModel());
+		System.out.println("Color : " + c1.getColor() );
+		System.out.println("Max Speed : " + c1.getMaxSpeed() + " km");
+		System.out.printf("Price : %,d won\n\n", c1.getPrice());
+		
+		System.out.println("Company : " + c2.getCompany());
+		System.out.println("Model : " + c2.getModel());
+		System.out.println("Color : " + c2.getColor() );
+		System.out.println("Max Speed : " + c2.getMaxSpeed() + " km");
+		System.out.printf("Price : %,d won\n\n", c2.getPrice());
+		
+		System.out.println("Company : " + c3.getCompany());
+		System.out.println("Model : " + c3.getModel());
+		System.out.println("Color : " + c3.getColor() );
+		System.out.println("Max Speed : " + c3.getMaxSpeed() + " km");
+		System.out.printf("Price : %,d won", c3.getPrice());
+		
+	}
+
+}
